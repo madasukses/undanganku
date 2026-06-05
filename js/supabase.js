8d2d2bf284a3f87be9e7f9e35553371b74923df9
Ahmada <ahmada.syafa@gmail.com>
2026-06-05 15:28:24 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,5 @@
-// js/supabase.js
-// Ganti dengan URL dan anon key dari Supabase Dashboard > Settings > API
-
-const SUPABASE_URL  = 'https://XXXXXXXXXXXXXXXX.supabase.co';
-const SUPABASE_ANON = 'eyXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const SUPABASE_URL  = 'https://dwrxehnyjfzighzysosw.supabase.co';
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3cnhlaG55amZ6aWdoenlzb3N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2MzYwMjUsImV4cCI6MjA5NjIxMjAyNX0.lPPsnhRpyDcFhUvrbxJEMc7DAjlM-BMyICOVi9KPqWc';
 
 const { createClient } = supabase;
 const sb = createClient(SUPABASE_URL, SUPABASE_ANON);
