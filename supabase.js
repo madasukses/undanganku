beaaffcb5aba7885163556d787026ae50c4a0259
Ahmada <ahmada.syafa@gmail.com>
2026-06-05 15:54:48 +0700

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -2,9 +2,4 @@ const SUPABASE_URL  = 'https://dwrxehnyjfzighzysosw.supabase.co';
 const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3cnhlaG55amZ6aWdoenlzb3N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2MzYwMjUsImV4cCI6MjA5NjIxMjAyNX0.lPPsnhRpyDcFhUvrbxJEMc7DAjlM-BMyICOVi9KPqWc';
 
 const { createClient } = supabase;
-const sb = createClient(SUPABASE_URL, SUPABASE_ANON, {
-  auth: {
-    detectSessionInUrl: true,
-    flowType: 'pkce'
-  }
-});
+const sb = createClient(SUPABASE_URL, SUPABASE_ANON);
